c5c326b2b1ce0d297444a3e4e0bdc75bc130d042
sppham <phamsyphuc123@gmail.com>
2023-01-16 00:51:00 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,3 +3,4 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
+from tensorflow.keras.layers import AveragePooling2D, GlobalAveragePooling2D, UpSampling2D, Reshape, Dense, LayerNormalization, Dropout, Attention
