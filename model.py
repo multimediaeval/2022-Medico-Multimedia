4b71d317e92967beef0419a579d3d8a34100c2f4
sppham <phamsyphuc123@gmail.com>
2023-01-16 00:50:30 +0900

    Update model.py

--- model.py
+++ model.py
@@ -2,3 +2,4 @@ import os
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
