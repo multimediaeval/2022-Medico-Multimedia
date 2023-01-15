1e7af9b2e9abbe5692fff2b922e2d376806e177b
sppham <phamsyphuc123@gmail.com>
2023-01-16 00:52:54 +0900

    Update model.py

--- model.py
+++ model.py
@@ -4,3 +4,16 @@ from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
 from tensorflow.keras.layers import AveragePooling2D, GlobalAveragePooling2D, UpSampling2D, Reshape, Dense, LayerNormalization, Dropout, Attention
+
+
+def SqueezeAndExcite(inputs, ratio=8):
+    init = inputs
+    filters = init.shape[-1]
+    se_shape = (1, 1, filters)
+
+    se = GlobalAveragePooling2D()(init)
+    se = Reshape(se_shape)(se)
+    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
+    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
+    x = init * se
+    return x
