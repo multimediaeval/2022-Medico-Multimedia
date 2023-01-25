a2cd13fe23cbc1a9eff98f48546948823c7184d3
sppham <phamsyphuc123@gmail.com>
2023-01-26 00:22:03 +0900

    Update TTA.py

--- TTA.py
+++ TTA.py
@@ -47,3 +47,77 @@ def save_results(ori_x, ori_y, y_pred, save_image_path):
 
     cat_images = np.concatenate([ori_x, line, ori_y, line, y_pred*255], axis=1)
     cv2.imwrite(save_image_path, cat_images)
+
+    
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    """ Folder for saving results """
+    create_dir("results_DLV3SA_inception")
+
+    """ Load the model """
+    with CustomObjectScope({'iou': iou, 'dice_coef': dice_coef}):
+        model = tf.keras.models.load_model("files_DLV3SA/model_DLV3SA_inception.h5")
+
+    """ Load the test data """
+    dataset_path = "D:/medical_challenge/segmentation/CVC-612/data/"
+    # dataset_path = "D:/medical_challenge/segmentation/2d/VIP_CUP_5fold/fold_1/train/"
+    # dataset_path = "D:/medical_challenge/segmentation/CVC-612/CVC_ClinicDB"
+    # dataset_path = "D:/medical_challenge/segmentation/2d/data_mbrain_5fold/fold_0/train/"
+    (train_x, train_y), (valid_x, valid_y) = load_data(dataset_path)
+    # print(len(test_x), len(test_y))
+    print("=====================================================================================================")
+    print(len(valid_x), len(valid_y))
+    print("=====================================================================================================")
+    print(len(train_x), len(train_y))
+    SCORE = []
+    for x, y in tqdm(zip(valid_x, valid_y), total=len(valid_x)):
+        """ Exctracting the image name """
+        # name = x.split("/")[-1]
+        # print(x)
+        name = x
+        """ Read the image and mask """
+        ori_x, x = read_image(x)
+        ori_y, y = read_mask(y)
+
+        """ Predicting the mask """
+        y_pred = model.predict(x)[0] > 0.5
+        y_pred = np.squeeze(y_pred, axis=-1)
+        y_pred = y_pred.astype(np.int32)
+
+        """ Saving the predicted mask """
+        save_image_path = os.path.join(os.getcwd(), 'results_DLV3SA_inception', os.path.basename(name))
+        ## os.getcwd: lấy path fiel hiện tại
+        ## basename: lấy tên ảnh
+
+        # print("*"*50, save_image_path)
+        save_results(ori_x, ori_y, y_pred, save_image_path)
+
+        """ Flatten the array """
+        y = y.flatten()
+        y_pred = y_pred.flatten()
+
+        """ Calculating metrics values """
+        # dice_coef_value = 1 - dice_loss(y, y_pred)
+        acc_value = accuracy_score(y, y_pred)
+        f1_value = f1_score(y, y_pred, labels=[0, 1], average="binary")
+        jac_value = jaccard_score(y, y_pred, labels=[0, 1], average="binary")
+        recall_value = recall_score(y, y_pred, labels=[0, 1], average="binary")
+        precision_value = precision_score(y, y_pred, labels=[0, 1], average="binary")
+        SCORE.append([name, acc_value, f1_value, jac_value, recall_value, precision_value])
+
+
+    """ mean metrics values """
+    score = [s[1:] for s in SCORE]
+    score = np.mean(score, axis=0)
+    # print(f"Dice score: {score[0]:0.5f}")
+    print(f"Accuracy: {score[0]:0.5f}")
+    print(f"F1: {score[1]:0.5f}")
+    print(f"Jaccard: {score[2]:0.5f}")
+    print(f"Recall: {score[3]:0.5f}")
+    print(f"Precision: {score[4]:0.5f}")
+
+    df = pd.DataFrame(SCORE, columns = ["Image Name", "Acc", "F1", "Jaccard", "Recall", "Precision"])
+    df.to_csv("files_DLV3SA/score_DLV3SA_inception.csv")
