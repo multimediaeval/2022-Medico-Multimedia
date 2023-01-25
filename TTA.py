d727121fd44b40be982735e3a40c7a3b5c42637d
sppham <phamsyphuc123@gmail.com>
2023-01-26 00:15:52 +0900

    Update TTA.py

--- TTA.py
+++ TTA.py
@@ -13,3 +13,37 @@ from tensorflow.keras.utils import CustomObjectScope
 from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score, recall_score
 from metrics import dice_loss, dice_coef, iou
 from train import create_dir, load_data
+
+
+H = 256
+W = 256
+
+def read_image(path):
+    x = cv2.imread(path, cv2.IMREAD_COLOR)  ## (H, W, 3)
+    x = cv2.resize(x, (W, H))
+    ori_x = x
+    x = x/255.0
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=0)
+    return ori_x, x                                ## (1, 256, 256, 3)
+
+
+def read_mask(path):
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  ## (H, W)
+    x = cv2.resize(x, (W, H))
+    ori_x = x
+    x = x/255.0
+    x = x.astype(np.int32)                    ## (256, 256)
+    return ori_x, x
+  
+def save_results(ori_x, ori_y, y_pred, save_image_path):
+    line = np.ones((H, 10, 3)) * 255
+
+    ori_y = np.expand_dims(ori_y, axis=-1)  ## (256, 256, 1)
+    ori_y = np.concatenate([ori_y, ori_y, ori_y], axis=-1) ## (256, 256, 3)
+
+    y_pred = np.expand_dims(y_pred, axis=-1)  ## (256, 256, 1)
+    y_pred = np.concatenate([y_pred, y_pred, y_pred], axis=-1) ## (256, 256, 3)
+
+    cat_images = np.concatenate([ori_x, line, ori_y, line, y_pred*255], axis=1)
+    cv2.imwrite(save_image_path, cat_images)
