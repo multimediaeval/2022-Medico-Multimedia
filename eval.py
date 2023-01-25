c6943120f2dbc2578345821f1caa17aac51e230a
sppham <phamsyphuc123@gmail.com>
2023-01-26 00:07:31 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -4,3 +4,13 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import tensorflow as tf
+import numpy as np
+import cv2
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow.keras.utils import CustomObjectScope
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score, recall_score
+from metrics import dice_loss, dice_coef, iou
+from train import create_dir, load_data
